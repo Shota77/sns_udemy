b673a84115fc215322f83f34ef8d436dec4cb18b
Shota77 <urano.shota1994@gmail.com>
2022-07-03 18:15:56 +0900

    I created classes. I created an instance of the account.

--- lib/View/time_line/time_line_page.dart
+++ lib/View/time_line/time_line_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sns_udemy/model/account.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -8,6 +9,17 @@ class TimeLinePage extends StatefulWidget {
 }
 
 class _TimeLinePageState extends State<TimeLinePage> {
+
+  Account myAccount = Account(
+    id: "1",
+    name: "Flutter labo",
+    userId: 'urano shota',
+    selfIntroduction: 'うい',
+    ImagePath: "https://www.apple.com/jp/shop/buy-iphone/iphone-13-pro/6.7%E3%82%A4%E3%83%B3%E3%83%81%E3%83%87%E3%82%A3%E3%82%B9%E3%83%97%E3%83%AC%E3%82%A4-1tb-%E3%82%B7%E3%82%A8%E3%83%A9%E3%83%96%E3%83%AB%E3%83%BC",
+    createdTime: DateTime.now(),
+    updatedTIme: DateTime.now(),
+  );
+
   @override
   Widget build(BuildContext context) {
       return Scaffold(
---
+++ lib/model/account.dart
@@ -0,0 +1,19 @@
+class Account {
+  String id;
+  String name;
+  String ImagePath;
+  String selfIntroduction;
+  String userId;
+  DateTime? createdTime;
+  DateTime? updatedTIme;
+
+  Account({
+    this.id = "",
+    this.name = "",
+    this.ImagePath = "",
+    this.selfIntroduction = "",
+    this.userId = "",
+    this.createdTime,
+    this.updatedTIme,
+  });
+}
